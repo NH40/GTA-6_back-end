8a8199a482f186a5e6006838370e71bb5f27b9ef
NH_Brodyaga <157802918+NH40@users.noreply.github.com>
2024-06-14 17:38:05 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+import cors from "cors"
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/api", async (req, res) => {
+	const {email, name} = req.body
+
+	if(!email || !name) return res.status(400).json({ message: "Email and Name required fils"})
+
+	try {
+
+		const createdRow = await prisma.waitList.create({
+			data: {
+				email,
+				name,
+			}, 
+		})
+		res.json(createdRow)
+	} catch (error) {
+		res.status(400).send({message: error})
+	}
+})
+
+const server = app.listen(PORT, () => {
+	console.log(`example app list port ${PORT}`)
+})
